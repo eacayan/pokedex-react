8c422def6507940fbb05651c392ac37c3c727d6b
Ellice Acayan <ellice_acayan@yahoo.com>
2019-04-23 19:34:46 +0800

    Updates

---
+++ precache-manifest.62cf3d0be83d40d7825e4670acbcbea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbbfe6c4106ff935e43d",
+    "url": "/static/css/main.3a408711.chunk.css"
+  },
+  {
+    "revision": "bbbfe6c4106ff935e43d",
+    "url": "/static/js/main.aaeb1331.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "31494845a71be6233c66",
+    "url": "/static/js/2.2b871f4e.chunk.js"
+  },
+  {
+    "revision": "79a69650fc55e1bc53e22b0ca3627081",
+    "url": "/static/media/pokeball.79a69650.png"
+  },
+  {
+    "revision": "b9ac6712f4a81502324bf40e07f709aa",
+    "url": "/static/media/background.b9ac6712.png"
+  },
+  {
+    "revision": "102f3954545daaec6850b01367200402",
+    "url": "/index.html"
+  }
+];
