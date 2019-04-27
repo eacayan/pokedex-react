6a616ef2cf87235c8b895ec6d9c8ee8e6fa65cd1
Ellice Acayan <ellice_acayan@yahoo.com>
2019-04-27 17:30:45 +0800

    updated text

--- src/components/home/PokeSearch.js
+++ src/components/home/PokeSearch.js
@@ -52,7 +52,7 @@ class PokeSearch extends Component {
       <div className="navbar-fixed">
         <nav>
           <div className="nav-wrapper red darken-2">
-            <a href="/" className="brand-logo left" style={{ paddingLeft: "2em" }}>Pokedex</a>
+            <a href="/" className="brand-logo left" style={{ paddingLeft: "2em" }}>Pokédex</a>
             <form
               onSubmit={this.onFormSubmit}
               className="right"
